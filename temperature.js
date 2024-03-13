d4bbb5539b1d01689c0807fde48b01846e79f71e
ranjith1444 <163101792+ranjith1444@users.noreply.github.com>
2024-03-13 10:09:35 +0530

    Add files via upload

---
+++ temperature.js
@@ -0,0 +1,11 @@
+function convertCelsiusToFahrenheit() {
+    var celsius = parseFloat(document.getElementById("celsius").value);
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit.toFixed(2);
+}
+
+function convertFahrenheitToCelsius() {
+    var fahrenheit = parseFloat(document.getElementById("fahrenheit").value);
+    var celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById("celsius").value = celsius.toFixed(2);
+}
